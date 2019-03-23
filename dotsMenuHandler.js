9c859015eee2b450f23d7bfdf23e26c51847ce4e
georgy4 <wallzy@mail.ru>
2019-03-24 02:48:35 +0300

    script updates

--- dotsMenuHandler.js
+++ dotsMenuHandler.js
@@ -1,29 +1,25 @@
 var dots = document.querySelectorAll('.dots');
-var contextMenu = document.querySelectorAll('.context-menu')
+var littleMenu = document.querySelectorAll('.context-menu')
 var menuItems = document.querySelectorAll('.immortal')
 
 var contextMenuHandler = function (c, d) {
   c.addEventListener('click', function(evt) {
     d.classList.toggle('context-menu-closed');
-    console.log(d.classList);
     evt.stopPropagation();
   });
 };
 
 for (var i = 0; i < dots.length; i++) {
   var a = dots[i];
-  var b = contextMenu[i];
+  var b = littleMenu[i];
   contextMenuHandler(a, b);
 }
 
-var contextMenuCloser = function (item, menu) {
     document.addEventListener('click', function(evt) {
-      if (evt.target !== item && evt.target !== menu) {
-      menu.classList.add('context-menu-closed');
+          if (evt.target !== menuItems[0] && evt.target !== menuItems[1] && evt.target !== menuItems[2] && evt.target !== menuItems[3] && evt.target !== menuItems[4] && evt.target !== menuItems[5] && evt.target !== menuItems[6] && evt.target !== menuItems[7] && evt.target !== menuItems[8] && evt.target !== menuItems[9] && evt.target !== menuItems[10] && evt.target !== menuItems[11] && evt.target !== menuItems[12] && evt.target !== menuItems[13] && evt.target !== menuItems[14]) {
+            for (var j = 0; j < littleMenu.length; j++) {
+              littleMenu[j].classList.add('context-menu-closed');
+        }   
       }
     });
-};
 
-for (var j = 0; j < contextMenu.length; j++) {
-  contextMenuCloser(menuItems[j], contextMenu[j]);
-}
